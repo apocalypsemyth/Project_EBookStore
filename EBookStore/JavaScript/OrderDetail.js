9aa39c93f6d0bfc604e139058d4c3cf95e724d7d
apocalypsemyth <accelerator.apocalypse@gmail.com>
2022-03-26 16:45:24 +0800

    加入取得使用者勾選哪個OrderBook的功能

--- EBookStore/JavaScript/OrderDetail.js
+++ EBookStore/JavaScript/OrderDetail.js
@@ -89,34 +89,44 @@
             AddShoppingCart(strOrBolBookID);
     });
 
-    //以下為使用者究其勾選哪一CheckBox的部分實作區
-    function getCheckedCheckBox() {
-        var selected = [];
+    function getCheckedBookID() {
+        var checkedBookID = [];
+
         $("#divOrderBookList label.list-group-item input:checked").each(function () {
-            selected.push($(this).attr('name'));
+            var aHref = $(this).siblings("a.btn").attr("href");
+            var strKeyValue = aHref.substring(aHref.indexOf("?") + 1);
+            var strValue = strKeyValue.split("=")[1];
+
+            checkedBookID.push(strValue);
         });
+
+        return checkedBookID.join();
     }
-    function DeleteShoppingCart(strBookID) {
+    function DeleteOrderBookInShoppingCart(strCheckedBookID) {
         $.ajax({
             url: "/API/OrderDetailDataHandler.ashx?Action=DELETE",
             method: "POST",
-            data: { "bookID": strBookID },
-            success: function (orderBookAmount) {
-                if (orderBookAmount === "0" || orderBookAmount === "NULL")
-                    return;
-                else {
-                    var shoppingCartBadgeHtml =
+            data: { "checkedBookID": strCheckedBookID },
+            success: function (bookList) {
+                var orderBookInShoppingCartHtml = "";
+                for (var book of bookList) {
+                    orderBookInShoppingCartHtml +=
                         `
-                            購物車
-                            <span id="spnshoppingcartbadge" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                ${orderBookAmount}
-                                <span class="visually-hidden">unread messages</span>
-                            </span>
+                            <label class="list-group-item">
+                                <input class="form-check-input me-1" type="checkbox" />
+                                <a class="btn" href="BookDetail.aspx?ID=${book.BookID}" title="前往查看：${book.BookName}">
+                                    <img class="card-img-top" src="${book.Image}" />
+                                    <div class="card-body">
+                                        <h5 class="card-title">書名:${book.BookName}
+                                        </h5>
+                                        <p class="card-text">${book.Price}</p>
+                                    </div>
+                                </a>
+                            </label>
                         `;
-
-                    $("#btnShoppingCart").empty();
-                    $("#btnShoppingCart").append(shoppingCartBadgeHtml);
                 }
+
+                $("#divOrderBookList").html(orderBookInShoppingCartHtml);
             },
             error: function (msg) {
                 console.log(msg);
@@ -124,9 +134,60 @@
             }
         });
     }
-    $("input[id*=ckbOrderBook]").click(function (e) {
+    $("#btnDeleteOrderBook").click(function (e) {
         e.preventDefault();
 
-        DeleteShoppingCart();
+        var strCheckedBookID = getCheckedBookID();
+        DeleteOrderBookInShoppingCart(strCheckedBookID);
+        BuildShoppingCartBadge();
+    });
+
+    function getSelectedPaymentID() {
+        var strSelectedPaymentID = "";
+
+        $("select[id*=ddlPaymentList]").select(function (e) {
+            strSelectedPaymentID = e.target.val();
+        });
+
+        return strSelectedPaymentID;
+    }
+    function CompleteOrder(numOrderStatus) {
+        $.ajax({
+            url: "/API/OrderDetailDataHandler.ashx?Action=UPDATE",
+            method: "POST",
+            data: { "orderStatus": numOrderStatus },
+            success: function (bookList) {
+                //var orderBookInShoppingCartHtml = "";
+                //for (var book of bookList) {
+                //    orderBookInShoppingCartHtml +=
+                //        `
+                //            <label class="list-group-item">
+                //                <input class="form-check-input me-1" type="checkbox" />
+                //                <a class="btn" href="BookDetail.aspx?ID=${book.BookID}" title="前往查看：${book.BookName}">
+                //                    <img class="card-img-top" src="${book.Image}" />
+                //                    <div class="card-body">
+                //                        <h5 class="card-title">書名:${book.BookName}
+                //                        </h5>
+                //                        <p class="card-text">${book.Price}</p>
+                //                    </div>
+                //                </a>
+                //            </label>
+                //        `;
+                //}
+
+                //$("#divOrderBookList").html(orderBookInShoppingCartHtml);
+            },
+            error: function (msg) {
+                console.log(msg);
+                alert("通訊失敗，請聯絡管理員。");
+            }
+        });
+    }
+    $("#btnCompleteOrder").click(function (e) {
+        e.preventDefault();
+
+        var strSelectedPaymentID = getSelectedPaymentID();
+        console.log(strSelectedPaymentID);
+        //CompleteOrder(1);
     })
 })
